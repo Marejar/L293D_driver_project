f003dd7985e97bf5c36a6d5e47d30d420f59c293
Marejar <Humble@interia.pl>
2023-04-01 16:53:04 +0200

    Added StartOrStop_motor function definition

--- Core/Inc/L293D_driver.h
+++ Core/Inc/L293D_driver.h
@@ -26,9 +26,9 @@ struct L293D_motor_t{
 
 	TIM_HandleTypeDef *htimer;
 	uint32_t timer_channel;
-	GPIO_TypeDef clockwise_rotation_enable_port;
+	GPIO_TypeDef *clockwise_rotation_enable_port;
 	uint8_t clockwise_rotation_enable_pin;
-	GPIO_TypeDef counterclockwise_rotation_enable_port;
+	GPIO_TypeDef *counterclockwise_rotation_enable_port;
 	uint8_t counterclockwise_rotation_enable_pin;
 	uint8_t direction_of_rotation_flag;
 
@@ -39,7 +39,7 @@ void L293D_motor_init(struct L293D_motor_t *pmotor, TIM_HandleTypeDef *htim, uin
 void EnorDi_pwm_signal(struct L293D_motor_t *pmotor, uint8_t EnorDi);
 void increase_motor_speed(struct L293D_motor_t *pmotor);
 void decrease_motor_speed(struct L293D_motor_t *pmotor);
-void StopOrStart_motor_f(struct L293D_motor_t *pmotor, uint8_t StoporStart);
-void select_motor_rotation_direction_f(struct L293D_motor_t *pmotor, uint8_t direction);
+void StopOrStart_motor(struct L293D_motor_t *pmotor, uint8_t StoporStart);
+void select_motor_rotation_direction(struct L293D_motor_t *pmotor, uint8_t direction);
 
 #endif /* INC_L293D_DRIVER_H_ */
