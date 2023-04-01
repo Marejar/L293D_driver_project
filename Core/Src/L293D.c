8358ab48edba73a29d6f9e04694d406b746fac98
Marejar <Humble@interia.pl>
2023-04-01 16:16:46 +0200

    Added decrease_motor_speed function's definition

--- Core/Src/L293D.c
+++ Core/Src/L293D.c
@@ -49,3 +49,36 @@ void increase_motor_speed(struct L293D_motor_t *pmotor){
 	}
 
 }
+
+
+void decrease_motor_speed(struct L293D_motor_t *pmotor){
+
+	uint32_t current_timer_pulse;
+	uint32_t demanded_timer_pulse;
+	uint32_t current_timer_period;
+
+	current_timer_pulse = pmotor->htimer->Instance->ARR;
+
+	if(pmotor->timer_channel == TIM_CHANNEL_1){
+		current_timer_period = pmotor->htimer->Instance->CCR1;
+	}else if(pmotor->timer_channel == TIM_CHANNEL_2){
+		current_timer_period = pmotor->htimer->Instance->CCR2;
+	}else if(pmotor->timer_channel == TIM_CHANNEL_3){
+		current_timer_period = pmotor->htimer->Instance->CCR3;
+	}else if(pmotor->timer_channel == TIM_CHANNEL_4){
+		current_timer_period = pmotor->htimer->Instance->CCR4;
+	}
+
+	demanded_timer_pulse = (current_timer_pulse - (current_timer_period * 0,1));
+
+	if(pmotor->timer_channel == TIM_CHANNEL_1){
+		pmotor->htimer->Instance->CCR1 = demanded_timer_pulse;
+	}else if(pmotor->timer_channel == TIM_CHANNEL_2){
+		pmotor->htimer->Instance->CCR2 = demanded_timer_pulse;
+	}else if(pmotor->timer_channel == TIM_CHANNEL_3){
+		pmotor->htimer->Instance->CCR3 = demanded_timer_pulse;
+	}else if(pmotor->timer_channel == TIM_CHANNEL_4){
+		pmotor->htimer->Instance->CCR4 = demanded_timer_pulse;
+	}
+
+}
